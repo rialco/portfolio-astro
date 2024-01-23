99fb51a9efbde4c04b15385144141ef0419f22ee
Ricardo <ralvarezcm@gmail.com>
2024-01-22 23:57:01 -0500

    refactor: use vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import vercel from "@astrojs/vercel/serverless";
 import solid from "@astrojs/solid-js";
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), solid()],
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
 });
